ad57fb59c6aba7f56a9495fc4be5ab4adbe6c376
Jacob Neher <jacob.neher@gmail.com>
2014-03-08 00:28:24 -0700

    Kill the grippies!

--- template.php
+++ template.php
@@ -6,3 +6,10 @@
  * Flintstones theme.
  */
 
+/**
+* Implements hook_preprocess_HOOK().
+*/
+function flintstones_preprocess_textarea(&$vars) {
+  // KILL THE GRIPPIE!!
+  $vars['element']['#resizable'] = 0;
+}
